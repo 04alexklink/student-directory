29d53c84d88cca8d3a98b8c7285bc24c0839f8b3
EC2 Default User <ec2-user@ip-172-31-19-83.eu-west-2.compute.internal>
2020-06-04 12:05:30 +0000

    SD challenge: Saving data to a file and .gitignore

--- Student-directory_challenge.rb
+++ Student-directory_challenge.rb
@@ -37,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more options.
 end
 
@@ -54,6 +55,9 @@ def process(selection)
     when "2"
       #show the students
       show_students
+    when "3"
+      #save the students
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else 
@@ -68,4 +72,17 @@ def interactive_menu
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort], student[:age], student[:hobbies]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 interactive_menu
+
